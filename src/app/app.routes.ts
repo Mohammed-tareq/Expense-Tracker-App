f1f62b94710448a8024ac210fbb3ebb780a090be
Mohammed tareq <Mohtareq1999m@gmail.com>
2025-06-11 10:20:14 +0300

    Handel Structure for project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BudgetDetailsComponent } from './pages/budget-details/budget-details.component';
+import { CreateAccountComponent } from './pages/create-account/create-account.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'details/:id', component: BudgetDetailsComponent },
+  { path: 'create-account', component: CreateAccountComponent }
+];
---
+++ src/app/pages/budget-details/budget-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-details',
+  imports: [],
+  templateUrl: './budget-details.component.html',
+  styleUrl: './budget-details.component.scss'
+})
+export class BudgetDetailsComponent {
+
+}
