c789a98b66d52e18d836410ad94b3209e1f2ed74
Mohammed tareq <Mohtareq1999m@gmail.com>
2025-06-11 13:11:11 +0300

    Refactor create account component and implement user service for account management

--- src/app/pages/create-account/create-account.component.ts
+++ src/app/pages/create-account/create-account.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-
+import { UserService } from '../../services/user.service';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-create-account',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.scss'
 })
 export class CreateAccountComponent {
+  constructor( private userService: UserService , private route: Router) { }
+  accountForm: FormGroup = new FormGroup({
+    name: new FormControl("", [Validators.required, Validators.minLength(3)]),
+  });
+  createAccount() {
 
+    this.userService.addUser(this.accountForm.value.name);
+    this.route.navigateByUrl('home');
+
+
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../interfaces/models/user';
+import { v4 as uuidv4 } from 'uuid';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  USER: string = 'user';
+
+  constructor(private router: Router) { }
+
+  addUser(name: string): void {
+    const user: User = {
+      id: uuidv4(),
+      name: name
+    }
+
+    localStorage.setItem(this.USER, JSON.stringify(user));
+  }
+
+  getUser(): User {
+    return JSON.parse(localStorage.getItem(this.USER) ?? '{}');
+  }
+
+  deleteUserAccount(): void {
+    localStorage.removeItem(this.USER);
+    this.router.navigateByUrl('create-account');
+  }
+
+  isLoggedIn() {
+
+    return Object.keys(this.getUser()).length > 0;
+  }
 }
