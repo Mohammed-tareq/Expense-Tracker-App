7c3206092fe62bae55672d9ed6458f47ed71a01b
Mohammed tareq <Mohtareq1999m@gmail.com>
2025-06-11 17:23:59 +0300

    Implement home component layout with budget and expense forms; add form wrapper component for better structure

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-
+import { UserService } from '../../services/user.service';
+import { FormWrapperComponent } from "../../components/form-wrapper/form-wrapper.component";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormWrapperComponent,ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  constructor( public userser:UserService){}
 
+  budgetForm:FormGroup = new FormGroup({
+    name: new FormControl('',[Validators.required, Validators.minLength(3)]),
+    budget: new FormControl(null,[Validators.required, Validators.min(1)]),
+  });
+  ExpanceForm:FormGroup = new FormGroup({
+    name: new FormControl('',[Validators.required, Validators.minLength(3)]),
+    amount: new FormControl(null,[Validators.required, Validators.min(1)]),
+    category: new FormControl(null,[Validators.required]),
+  });
 }
+
